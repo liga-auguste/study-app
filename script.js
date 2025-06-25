eee5719d3ba19fe2edc158e44e29900e6ab08900
Liga Auguste <ligaauguste@gmail.com>
2025-06-25 16:29:36 +0200

    function for input fields added, html editable lecture updated

--- script.js
+++ script.js
@@ -13,3 +13,42 @@ function generateLecture() {
 
   unitCounter++;
 }
+
+function addListItem(containerId) {
+  const container = document.getElementById(containerId);
+
+  // Neues Listenelement
+  const li = document.createElement("li");
+
+  // Wrapper für Input + Button
+  const itemWrapper = document.createElement("div");
+  itemWrapper.className = "input-wrapper";
+
+  // Eingabefeld
+  const input = document.createElement("input");
+  input.type = "text";
+  input.name = "begriffe[]";
+  input.className = "begriff-input longInput";
+
+  // Optional: Input beim Fokus leeren
+  input.onfocus = function () {
+    this.value = '';
+  };
+
+  // Entfernen-Button
+  const removeBtn = document.createElement("button");
+  removeBtn.type = "button";
+  removeBtn.textContent = "✕";
+  removeBtn.className = "remove-btn";
+
+  removeBtn.onclick = function () {
+    container.removeChild(li);
+  };
+
+  // Elemente zusammensetzen
+  itemWrapper.appendChild(input);
+  itemWrapper.appendChild(removeBtn);
+  li.appendChild(itemWrapper);
+  container.appendChild(li);
+}
+
