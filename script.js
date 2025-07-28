73e0645371c369a336588f667c2ec3930c657b41
Liga Auguste <ligaauguste@gmail.com>
2025-07-28 14:50:21 +0200

    modal added

--- script.js
+++ script.js
@@ -141,4 +141,11 @@ window.addEventListener("DOMContentLoaded", () => {
   }
 });
 
+const openBtn = document.querySelector('#open-modal');
+const dialog = document.querySelector('#dialog');
+const closeBtn = document.querySelector('#close-modal');
+
+openBtn.addEventListener('click', () => dialog.showModal());
+closeBtn.addEventListener('click', () => dialog.close());
+
 renderLectures();
